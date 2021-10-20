51225e36dc6336750cd10fd40658cbff827fad0c
chanwoo95 <chanwoo95@github.com>
2021-10-20 12:55:23 +0900

    login component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import react from "react";
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import Login from "./component/login/login";
+import styles from "../src/app.module.css";
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path="/">
-          <div>
+    <div className={styles.app}>
+      <Router>
+        <Switch>
+          <Route path="/">
             <Login />
-          </div>
-        </Route>
-      </Switch>
-    </Router>
+          </Route>
+        </Switch>
+      </Router>
+    </div>
   );
 }
 
--- src/component/login/login.jsx
+++ src/component/login/login.jsx
@@ -1,5 +1,19 @@
+import styles from "../login/login.module.css";
+
 function Login() {
-  return <h2>Login</h2>;
+  return (
+    <div>
+      <header>
+        <div>logo img</div> {/* 로고 이미지 추가요망 */}
+      </header>
+      <section className={styles.section}>
+        <input type="text" placeholder="아이디" />
+        <input type="text" placeholder="패스워드" />
+      </section>
+      <button className={styles.button}>Login</button>
+      <footer>2021. re-member</footer>
+    </div>
+  );
 }
 
 export default Login;
