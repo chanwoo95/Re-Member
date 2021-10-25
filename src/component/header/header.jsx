15001122d6b6e7bc403d4ea7dbc40a1c4909600b
chanwoo95 <cksdndbd95@gmail.com>
2021-10-25 19:52:18 +0900

    login page css

--- src/component/header/header.jsx
+++ src/component/header/header.jsx
@@ -2,9 +2,16 @@ import react from "react";
 import styles from "./header.module.css";
 
 function Header() {
+  const onLogout = () => false;
+
   return (
     <header className={styles.header}>
       <h1 className={styles.title}>re.member</h1>
+      {onLogout && (
+        <button className={styles.logout} onClick={onLogout}>
+          Logout
+        </button>
+      )}
     </header>
   );
 }
