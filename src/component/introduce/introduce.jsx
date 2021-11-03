62ee72b953b9fdbd02393d538c0b164ea4bcf627
chanwoo95 <cksdndbd95@gmail.com>
2021-11-03 21:19:27 +0900

    ...

---
+++ src/component/introduce/introduce.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Introduce = (props) => <h2>회사소개</h2>;
+
+export default Introduce;
--- src/component/mainhome/mainhome.jsx
+++ src/component/mainhome/mainhome.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import styles from "./mainhome.module.css";
 import Nav from "../nav/nav";
+import Introduce from "../introduce/introduce";
 
 const MainHome = (props) => (
-  <div className={styles.container}>
-    <Nav />
-  </div>
+  <>
+    <div className={styles.nav}>
+      <Nav />
+    </div>
+    <div className={styles.introduce}>
+      <Introduce />
+    </div>
+  </>
 );
 
 export default MainHome;
