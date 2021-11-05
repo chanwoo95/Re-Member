b7f3bbbeeda8d3d7c2e604e7e858ae9e78ead522
chanwoo95 <cksdndbd95@gmail.com>
2021-11-05 09:10:07 +0900

    side navigation

--- src/components/side-navigation.jsx
+++ src/components/side-navigation.jsx
@@ -1,6 +1,30 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const SideNavigation = () => {};
+const SideNavigation = () => {
+  <div>
+    <header>
+      <Link to="/">
+        <h2>Re.Member</h2>
+      </Link>
+    </header>
+    <div className="navGroup">
+      <ul>
+        <Link to="/">
+          <li>Home</li>
+        </Link>
+        <Link to="/search">
+          <li>Search</li>
+        </Link>
+        <Link to="/work">
+          <li>Work</li>
+        </Link>
+      </ul>
+    </div>
+    <footer>
+      <div>state</div>
+    </footer>
+  </div>;
+};
 
 export default SideNavigation;
